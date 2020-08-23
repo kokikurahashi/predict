5ffc1dd4e610be46148c169b12200556ab1eb8f6
kurahashi <koki.kurahashi@di-v.co.jp>
2020-08-23 21:28:13 +0900

    use_cuda

--- predict_price.py
+++ predict_price.py
@@ -3,13 +3,13 @@ import numpy as np
 import torch
 import math
 import time
-filename = "./7203_2019.csv"
+filename = "./4755_2018.csv"
 csv_file = open(filename, "r", encoding="ms932", errors="", newline="" )
 #リスト形式
 f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 loop = 2
 kernel_size = 10000
-learning_rate = 0.0000001
+learning_rate = 0.0001
 
 for itr in range(loop):
   next(f)
@@ -53,14 +53,16 @@ def train(date,latest_price,learning_rate,kernel_size,theta,model):
     # y += math.sin(2*date*math.pi/(i+3))*params_sin[i]+math.cos(2*date*math.pi/(i+3))*params_cos[i]
   # t2 = time.time()
   # print(0,t2-t1)
-  MSELoss = nn.MSELoss()
-  optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate) 
+  MSELoss = nn.L1Loss()
+  beta1 = 0.5
+  optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=(beta1, 0.999), weight_decay=1e-5) 
   p = torch.zeros(1).cuda(0)
   p[0] = latest_price
   MSE = MSELoss(y,p)
+  loss = MSE
   if date == 200:
     print(MSE)
-  MSE.backward() # 誤差逆伝播
+  loss.backward() # 誤差逆伝播
   optimizer.step()  # Generatorのパラメータ更新
   model.zero_grad() 
   return model
@@ -73,8 +75,8 @@ print(theta.shape)
 
   
 #100回パラメータを学習
-for loop in range(300):
-  for i in range(latest_price_list.shape[0]-40):
+for loop in range(1000):
+  for i in range(latest_price_list.shape[0]-140):
     theta = 2*math.pi*torch.ones(kernel_size)/torch.arange(3,kernel_size+3,1)
     model = train(i+1,latest_price_list[i],learning_rate,kernel_size,theta,model)
 
@@ -82,13 +84,13 @@ y_list = []
 for itr in range(latest_price_list.shape[0]):
   y = 0
   theta = 2*math.pi*torch.ones(kernel_size)/torch.arange(3,kernel_size+3,1)
-  theta = theta*date
-  y = model(z = torch.cat((torch.sin(theta),torch.cos(theta))))
+  theta = theta*itr
+  y = model(torch.sin(theta).cuda(0),torch.cos(theta).cuda(0))
   y = y.cpu().detach().numpy()
   y_list.append(y)
 
-# text_file = open("./drive/My Drive/sample_writ_er_row.csv", "wt")
+text_file = open("./drive/My Drive/sample_writ_er_row.csv", "wt")
 for y in y_list:
-      # text_file.write(str(y)+"\n")
+      text_file.write(str(y)+"\n")
       print(str(y))
-# text_file.close()
+text_file.close()
